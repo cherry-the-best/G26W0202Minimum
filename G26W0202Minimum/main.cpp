7c372974aa2b6848bad2669acf1b06b22393045a
cherry-the-best <cherrycprhdi@naver.com>
2026-09-16 18:58:22 +0900

    ✨메시지 처리 (원그리기)

--- G26W0202Minimum/main.cpp
+++ G26W0202Minimum/main.cpp
@@ -1,28 +1,50 @@
 ﻿#include <afxwin.h>
-//CWinApp app;  // 반드시 주석 처리할 것!!!!!!
 
 class MyApp : public CWinApp {
 public:
-	virtual BOOL InitInstance();
+    virtual BOOL InitInstance();
 };
 
-class CMainWnd : public CFrameWnd {     // 내가 만든 건 Main 재정의 : 상속 마소에서 만든 게 Frame 
+class CMainWnd : public CFrameWnd {
+private:
+    CPoint m_ptClick = CPoint(-100, -100);
+
 public:
-	CMainWnd();
+    CMainWnd();
+
+    DECLARE_MESSAGE_MAP()     // 매크로
+
+        afx_msg void OnLButtonDown(UINT nFlags, CPoint point);              // 마우스 왼쪽 버튼 
+    afx_msg void OnPaint();                   // 화면 다시 그릴 때 
 };
 
 BOOL MyApp::InitInstance() {
-	//AfxMessageBox(L"파생 클래스의 InitInstance() 재정의");
+    m_pMainWnd = new CMainWnd();
+    m_pMainWnd->ShowWindow(m_nCmdShow);
+    m_pMainWnd->UpdateWindow();
 
-	m_pMainWnd = new CMainWnd();
-	m_pMainWnd->ShowWindow(m_nCmdShow);
-	m_pMainWnd->UpdateWindow();
-
-	return TRUE;
+    return TRUE;
 }
 
+BEGIN_MESSAGE_MAP(CMainWnd, CFrameWnd)
+    ON_WM_LBUTTONDOWN()
+    ON_WM_PAINT()
+END_MESSAGE_MAP()
+
 CMainWnd::CMainWnd() {
-	Create(NULL, L"GUI 프로그래밍");
+    Create(NULL, L"GUI 프로그래밍 - 원 그리기");
+}
+
+void CMainWnd::OnLButtonDown(UINT nFlags, CPoint point) {
+    m_ptClick = point;
+    Invalidate();             // 이거 중요함 ! 해야 돌아가니까 ! 
+}
+
+void CMainWnd::OnPaint() {
+    CPaintDC dc(this);
+
+    dc.Ellipse(m_ptClick.x - 30, m_ptClick.y - 30,
+        m_ptClick.x + 30, m_ptClick.y + 30);
 }
 
 MyApp app;
